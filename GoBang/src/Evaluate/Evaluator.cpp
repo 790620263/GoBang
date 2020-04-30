283dca784ce6ea4b6de1c43daa41f70824ed6c55
AtCraft <790620263@qq.com>
2020-05-01 03:40:43 +0800

    可行位置查找

--- GoBang/src/Evaluate/Evaluator.cpp
+++ GoBang/src/Evaluate/Evaluator.cpp
@@ -21,13 +21,31 @@ Position* Evaluator::getAvailablePosition(Board& b,int &size)
 		for (int y = 0; y < boardSize; y++)
 		{
 			int code = b.getPlayerCode(x, y);
-			//if (code != 0)
-			//{//周围9*9可行
-			//	for(int i=-1;i<2;i++)
-			//		for(int j=-1;j<2;j++)
-			//}
-			if (code == 0)plist[size] = Position{ x,y };
-			size++;
+			if (code != 0)
+			{//周围3*3可行
+				for(int i=-1;i<2;i++)
+					for (int j = -1; j < 2; j++)
+					{
+						if (i != 0 || j != 0)
+						{
+							int tmpx = x + i, tmpy = y + j; 
+							bool notexist = true;
+							if (b.getPlayerCode(tmpx, tmpy) == 0) {
+								for (int k = 0; k < size; k++)
+								{
+									if (plist[k].x == tmpx && plist[k].y == tmpy) { notexist = false; break; }
+								}
+								if (notexist)
+								{
+									plist[size] = Position{ tmpx,tmpy };
+									size++;
+								}
+							}
+						}
+					}
+				
+			}
+			//if (code == 0)plist[size] = Position{ x,y };
 		}
 	}
 	return plist;
